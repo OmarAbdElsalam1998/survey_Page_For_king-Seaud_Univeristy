f7ce8b683eef31b958fe31a3ee4cc3b236ca7bd0
OmarAbdElsalam1998 <O.Abdulsalam16.121@compit.au.edu.eg>
2023-10-14 00:49:01 +0300

    upload

--- assets/js/custon_en.js
+++ assets/js/custon_en.js
@@ -226,7 +226,7 @@ submit_button.addEventListener("click",(e)=>{
 
 function sendEmail(){
     (function(){
-        emailjs.init("fpJZ-8MzxdLM1ChIu");
+        emailjs.init("sKV7QyH9TsieomS2K");
     })();
     let serviceID="service_ssu5aus";//email service id
     let templateID="template_077uiv7";//email template id 
